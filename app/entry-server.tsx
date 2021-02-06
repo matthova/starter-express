6b6186f7aa0b0fa44f78c22e306ae0402fe23c46
Matt Hova <matthova87@gmail.com>
2021-02-06 10:27:41 -0700

    MVP styled-components working :tada:

--- app/entry-server.tsx
+++ app/entry-server.tsx
@@ -1,4 +1,3 @@
-import ReactDOMServer from "react-dom/server";
 import type { EntryContext } from "@remix-run/core";
 import Remix from "@remix-run/react/server";
 import { renderToString } from "react-dom/server";
@@ -10,37 +9,26 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  let markup = ReactDOMServer.renderToString(
-    <Remix context={remixContext} url={request.url} />
+  const sheet = new ServerStyleSheet();
+
+  let markup = renderToString(
+    sheet.collectStyles(<Remix context={remixContext} url={request.url} />)
   );
 
-  const sheet = new ServerStyleSheet();
-  let styleHeader = "";
-  try {
-    const html = renderToString(
-      sheet.collectStyles(<Remix context={remixContext} url={request.url} />)
-    );
-    styleHeader = sheet
-      .getStyleTags()
-      // remove the <style> tags?
-      .replace(/(<([^>]+)>)/gi, "")
-      // remove line breaks to be a valid Fetch header string
-      .replace(/\n/g, "");
-  } catch (error) {
-    // handle error
-    console.error(error);
-  } finally {
-    sheet.seal();
-  }
-
-  console.log("Our styles: ", styleHeader);
+  // get the styles
+  const styles = sheet
+    .getStyleTags()
+    // remove the <style> tags
+    .replace(/(<([^>]+)>)/gi, "");
+  const styleData = Buffer.from(styles).toString("base64");
+  sheet.seal();
 
   return new Response("<!DOCTYPE html>" + markup, {
     status: responseStatusCode,
     headers: {
       ...Object.fromEntries(responseHeaders),
       "Content-Type": "text/html",
-      style: styleHeader,
+      Link: `<data:text/css;utf-8;base64,${styleData}>; REL=stylesheet`,
     },
   });
 }
